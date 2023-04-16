a5487aad5dd1a8566304bed4d8415da3e239121c
upccore <upccore@list.ru>
2023-04-16 16:29:24 +0300

    End of working day 16.04.2023

--- src/main/java/com/example/fxproject/FXProjectController.java
+++ src/main/java/com/example/fxproject/FXProjectController.java
@@ -1,27 +1,49 @@
 package com.example.fxproject;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class FXProjectController {
 
     Person person = new Person();
+    StringBuilder result = new StringBuilder();
 
     @FXML
-    private Label mainText;
+    private ComboBox<String> comboList;
 
     @FXML
-    private Button nextButton;
+    private TextField ageTextArea;
 
     @FXML
-    private TextArea textArea;
+    private TextField nameTextArea;
 
     @FXML
-    void onNextButtonClick() throws InterruptedException {
-        person.setName(textArea.getText());
-        textArea.clear();
+    void onSaveButton() {
+        person.setName(nameTextArea.getText());
+        person.setAge(ageTextArea.getText());
+        person.setGender(comboList.getValue());
+
+        result.append("Имя и фамилия: ").append(person.getName()).append("\n");
+        result.append("Возраст: ").append(person.getAge()).append("\n");
+        result.append("Пол: ").append(person.getGender()).append("\n");
+
+        File file = new File("C:\\Users\\upcco\\OneDrive\\Рабочий стол\\" + person.getName() + ".txt");
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.write(result.toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void initialize() {
+        ObservableList<String> list = FXCollections.observableArrayList("мужской", "женский");
+        comboList.setItems(list);
     }
 }
 
