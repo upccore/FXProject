f30467104a9c56683466567cb18af30cdd54028c
upccore <upccore@list.ru>
2023-04-17 17:50:56 +0300

    CheckBoxes added

--- src/main/java/com/example/fxproject/FXProjectController.java
+++ src/main/java/com/example/fxproject/FXProjectController.java
@@ -3,6 +3,7 @@ package com.example.fxproject;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
@@ -15,23 +16,38 @@ public class FXProjectController {
     StringBuilder result = new StringBuilder();
 
     @FXML
-    private ComboBox<String> comboList;
+    private TextField nameTextArea;
 
     @FXML
     private TextField ageTextArea;
 
     @FXML
-    private TextField nameTextArea;
+    private TextField cityTextArea;
+
+    @FXML
+    private ComboBox<String> comboEducation;
+
+    @FXML
+    private CheckBox visualV;
+
+    @FXML
+    private CheckBox audioV;
+
+    @FXML
+    private CheckBox kinesteticV;
+
+    @FXML
+    private CheckBox audiodigitV;
 
     @FXML
     void onSaveButton() {
         person.setName(nameTextArea.getText());
         person.setAge(ageTextArea.getText());
-        person.setGender(comboList.getValue());
+        person.setCity(cityTextArea.getText());
+        person.setEducation(comboEducation.getValue());
 
         result.append("Имя и фамилия: ").append(person.getName()).append("\n");
         result.append("Возраст: ").append(person.getAge()).append("\n");
-        result.append("Пол: ").append(person.getGender()).append("\n");
 
         File file = new File("C:\\Users\\upcco\\OneDrive\\Рабочий стол\\" + person.getName() + ".txt");
         try (FileWriter fileWriter = new FileWriter(file)) {
@@ -42,8 +58,11 @@ public class FXProjectController {
     }
 
     public void initialize() {
-        ObservableList<String> list = FXCollections.observableArrayList("мужской", "женский");
-        comboList.setItems(list);
+        ObservableList<String> list = FXCollections.observableArrayList
+                ("дошкольное образование", "начальное общее образование","основное общее образование"
+                ,"среднее общее образование", "среднее профессиональное образование", "высшее образование - бакалавриат"
+                , "высшее образование - специалитет, магистратура", "высшее образование - подготовка кадров высшей квалификации");
+        comboEducation.setItems(list);
     }
 }
 
--- src/main/java/com/example/fxproject/Person.java
+++ src/main/java/com/example/fxproject/Person.java
@@ -4,7 +4,60 @@ public class Person {
 
     private String name;
     private String age;
-    private String gender;
+    private String city;
+    private String education;
+    private String representSystemVisual;
+    private String representSystemAudio;
+    private String representSystemKinestetic;
+    private String representSystemAudiodigit;
+
+    public String getRepresentSystemVisual() {
+        return representSystemVisual;
+    }
+
+    public void setRepresentSystemVisual(String representSystemVisual) {
+        this.representSystemVisual = representSystemVisual;
+    }
+
+    public String getRepresentSystemAudio() {
+        return representSystemAudio;
+    }
+
+    public void setRepresentSystemAudio(String representSystemAudio) {
+        this.representSystemAudio = representSystemAudio;
+    }
+
+    public String getRepresentSystemKinestetic() {
+        return representSystemKinestetic;
+    }
+
+    public void setRepresentSystemKinestetic(String representSystemKinestetic) {
+        this.representSystemKinestetic = representSystemKinestetic;
+    }
+
+    public String getRepresentSystemAudiodigit() {
+        return representSystemAudiodigit;
+    }
+
+    public void setRepresentSystemAudiodigit(String representSystemAudiodigit) {
+        this.representSystemAudiodigit = representSystemAudiodigit;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
 
     public String getAge() {
         return age;
@@ -12,14 +65,6 @@ public class Person {
 
     public void setAge(String age) {
         this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 
     public String getName() {
